0a6eee1716b6e9feb51f457493b66f37a1997908
zhenrong1999 <zhenrong1999@student.usm.my>
2018-10-01 19:33:03 +0800

    Timestamp is modified to have shorter interval for refreshing. Menu is done. Menu 1 is done (left some bug). Add exit for the program.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <stdlib.h>
 #include <thread>
+#include <string>
 
 
 using namespace std;
@@ -16,20 +17,24 @@ void loop() {
 
     while(!stop)
     {
+
             time (&rawtime);
             timeinfo = localtime (&rawtime);
             strftime (buffer,80,"Now it's %I:%M%p. %x",timeinfo );
-            cout << "Welcome to Intelligent Vending Machine!"<< endl ;
-            puts (buffer);
-            x++;
-            _sleep(1000);
-            system ("CLS");
+            cout << "Welcome to Intelligent Vending Machine!"<< flush << endl ;
+            cout << buffer << flush << endl ;
+            _sleep(100);
+
+            system("CLS");
+
+
     }
 
 }
 
 int main()
 {
+
 
     thread t(loop); // Separate thread for loop.
     // This actually starts a thread.
@@ -39,6 +44,52 @@ int main()
     // Set the boolean to true. The loop thread will exit from
     // loop and terminate.
     stop = true;
+
+    //Show Menu
+    int no_menu=0;
+    double eWallet=0.00;
+    _sleep(150);
+    system("CLS");
+
+    while (stop == true)
+    {
+        system("CLS");
+        cout << "Current amount in eWallet: RM" << eWallet   << endl;
+        cout << "1. Depositing into eWallet\n" ;
+        cout << "2. Purchase an item\n" ;
+        cout << "3. Display the item purchased and amount due\n" ;
+        cout << "4. Display the balance or change in the most efficient that is least Ringgit Malaysian denomination.\n" ;
+        cout << "5. Display currency exchange rate and price of item the balance in the currency\n" ;
+        cout << "6. Checkout\n" ;
+        cout << "7. Exit\n" ;
+        cin  >>  no_menu; //key no of menu
+
+        if (no_menu <=0)
+        {
+            cout << "You have crash the system. Please restart the system....................";
+            return 0;
+        }
+
+        if (no_menu==1) //Program for depositing
+            {
+                double keyin;
+                cout <<"Deposit for eWallet: RM ";
+                cin >> keyin;
+                if (keyin+eWallet>10000) //Limit for the eWallet
+                {
+                    _sleep(100);
+                    cout << "Warning!!!";
+                    cout << "You have deposit too much or your wallets full.\n";
+                    cin.get();
+
+                }
+                    else {eWallet=eWallet+keyin;}
+            }
+
+    }
+
+
+
 
 
     return 0;
