933db28d9cf5ce938aec6fa47f03835d7f271118
zhenrong1999 <zhenrong1999@student.usm.my>
2018-10-03 23:57:48 +0800

    Major change. Added menu 2,3,6and 7. secret code and add cart system added. Left minor changes and menu 4&5.

--- main.cpp
+++ main.cpp
@@ -1,112 +1,102 @@
 #include <iostream>
 #include <ctime>
-#include <chrono>
-
-#include <thread>
 #include <string>
 #include <cstdlib>
+#include <iomanip>
 
 
 using namespace std;
-bool stop = false; //the boolean stop is created before void so that the stop become public.
-void loop() {
 
-    time_t rawtime;
-    struct tm * timeinfo;
-    char buffer [80];
-    int x=0;
-
-    while(!stop)
-    {
-
-            time (&rawtime);
-            timeinfo = localtime (&rawtime);
-            strftime (buffer,80,"Now it's %I:%M%p. %x",timeinfo );
-            cout << "Welcome to Intelligent Vending Machine!"<< flush << endl ;
-            cout << buffer << flush << endl ;
-            _sleep(100);
-            system("CLS");
-
-
-
-    }
-
-}
 
 int main()
 {
-
-    thread t(loop); // Separate thread for loop.
-    // This actually starts a thread.
-    // Wait for input character (this will suspend the main thread, but the loop
-    // thread will keep running).
-    cin.get();
-    // Set the boolean to true. The loop thread will exit from
-    // loop and terminate.
-    stop = true;
-    bool stop2=false;
-
-    //Show Menu
+    time_t rawtime;
+    struct tm * timeinfo;
+    char buffer [80];
     int no_menu=0;
     double eWallet=0.00;
-    _sleep(150);
-    system("CLS");
+    double price;
+    string item;
+    bool entered = false;
+    bool form_menu2 = false;
+    int buff_acc;
+    int buff_pass;
+    string buff_passS;
 
-    while (1==1)
+
+
+        time (&rawtime);
+        timeinfo = localtime (&rawtime);
+        strftime (buffer,80,"Now it's %I:%M%p. %x",timeinfo );
+        cout << buffer << endl ;
+
+while (1==1)
     {
-        system("CLS");
+        //Show Menu
+    do
+        {system("CLS");
+        time (&rawtime);
+        timeinfo = localtime (&rawtime);
+        strftime (buffer,80,"Now it's %I:%M%p. %x",timeinfo );
+        cout << buffer << endl ;
+        if (entered==false)
+                {
+                    cout << "Welcome to Intelligent Vending Machine!"<< endl ;
+                    cin.get();
+                    cout << "Please enter your account number.\n";
+                    cin >> buff_acc;
+                    cout << "Please enter your pin number.\n";
+                    cin.clear();cin.ignore();
+                    getline(cin,buff_passS) ;
+                    buff_pass=stoi(buff_passS);
+                    system("cls");
+                    no_menu=7;
+
+
+                }
+        else
+        {
+        cout<<fixed<<showpoint<<setprecision(2);;
+        cout << "Welcome back!\n"<<"Now,you are now using Intelligent Vending Machine."<< endl ;
         cout << "Current amount in eWallet: RM" << eWallet   << endl;
         cout << "1. Depositing into eWallet\n" ;
         cout << "2. Purchase an item\n" ;
         cout << "3. Display the item purchased and amount due\n" ;
         cout << "4. Display the balance or change in the most efficient that is least Ringgit Malaysian denomination.\n" ;
         cout << "5. Display currency exchange rate and price of item the balance in the currency\n" ;
-        cout << "6. Checkout\n" ;
-        cout << "7. Exit\n" ;
+        cout << "6. Exit\n" ;
         cin  >>  no_menu; //key no of menu
+        system("cls");
 
         if (no_menu <=0)
         {
-            cout << "You have crash the system. Please restart the system....................";
-            system("start Intelligent Vending Machine.exe");
-            return 0;
-        }
-        if(no_menu ==7)
-        {
-            cout << "Thank you for using the Intelligient Vending Machine.\n";
-            cout << "See you in the next transaction.\n";
-            _sleep (2000);
-            stop=false;
-            thread t(loop);
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "You have crash the system. Restarting the system....................\n";
+            cin.clear();cin.ignore();
             cin.get();
-            stop=true;
-
-            return 1;
-
+            entered=false;
         }
-        if (no_menu==2 & eWallet>0 )
-            {
-                cout << " Here are the HOT item that are in sale right now. \n";
-                cout << "-Apple Macbook Pro\n";
-                cout << "-Seagate 250G SSD\n";
-                cout << "-MSI Gaming Laptop\n";
-                cout <<"-Cosiaer Gaming Mouse\n";
-                cout <<"-Dell XPS 15\n";
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                string item=;
-                getline(cin, item);
-            }
-        if (no_menu==2 && eWallet<0 )
-                {cout << "Please deposit more in order to buy more great staff. (Press 1)";cin.ignore(numeric_limits<streamsize>::max(), '\n');cin.get(); }
+        }
+        if(no_menu==7)
+        {
+            int sec_code;
+            int x ;
+            int buff;
+            int buff2;
+            entered=true;
+            for (x=0;x<=3;x++)
+            {//secret sauce ha ha ha :)
+                buff2=pow(10,x+1);
+                buff = (buff_pass%buff2) / (pow(10,x)); //Equation finding the place value
+                if((buff % 2)>=0.5){buff=(buff+7)%10;}  //Find Odd numbe
+                else {buff=(buff+11)%10;}//Find even number                   r
+                sec_code=sec_code+buff*pow(10,3-x);
+               }
+            cout << sec_code;
+            cin.clear();cin.ignore();
+            cin.get();
+        }
 
-        if (no_menu==2 && eWallet==0 )
-            {cout<<"Please deposit first in order to proceed! (Press 1) \n";cin.ignore(numeric_limits<streamsize>::max(), '\n');cin.get();}
 
-        if (no_menu==3 )
-            {
-                cout <<  item_name;
-            }
 
         if (no_menu==1) //Program for depositing
          {
@@ -118,14 +108,75 @@ int main()
 
                     cout << "Warning!!!";
                     cout << "You have deposit too much or your wallets full.\n";
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cin.clear();cin.ignore();
                     cin.get();
 
                 }
                     else {eWallet=eWallet+keyin;}
-          }
          }
-}
+        if(no_menu ==6)
+        {
+            cout << "Thank you for using the Intelligent Vending Machine.\n";
+            cout << "See you in the next transaction.\n";
+            entered=false;
+            cin.clear();cin.ignore();
+
+        }
+        if (no_menu==2 && eWallet>0 )
+            {
+                cout << " Here are the HOT item that are in sale right now. \n";
+                cout << "-Apple Macbook Pro\n";
+                cout << "-Seagate 250G SSD\n";
+                cout << "-MSI Gaming Laptop\n";
+                cout <<"-Cosiaer Gaming Mouse\n";
+                cout <<"-Dell XPS 15\n";
+                cout << "\nYou can buy almost anything except things that are impossible to buy like time and life. \n";
+                cout << "Please don't buy things that are prohibited under the law.\nFollow the rule of law.\n";
+                cin.clear();cin.ignore();    //for clearing the cin        //ignore(numeric_limits<streamsize>::max(), '\n');
+                getline(cin, item);
+                srand(time(0)) ;
+                price= 1+(rand()%5530);
+                price=price/100;
+                form_menu2=true;
+                cout << "\n\n\n"<<"Please check the item list below carefully.\n\n";
+                no_menu=3;
+            }
+        if (no_menu==2 && eWallet<0 )
+            {cout << "Please deposit more in order to buy more great staff. (Press 1)";cin.ignore(numeric_limits<streamsize>::max(), '\n');cin.get(); }
+
+        if (no_menu==2 && eWallet==0 )
+            {cout<<"Please deposit first in order to proceed! (Press 1) \n";cin.ignore(numeric_limits<streamsize>::max(), '\n');cin.get();}
+
+        if (no_menu==3)
+            {
+                if (price<=0)
+                     {cout<<"You have come to the wrong place."<<"Come back after you have purchase something interesting."<<"Please an key to teleport you back to the main menu"; cin.clear();cin.ignore(); cin.get();}
+               else{
+                cout << "The items that you have brought.";
+
+
+                cout << "Name of item: "<<  item <<endl;
+                cout << "Price in RM: RM" <<  price<<endl;
+
+
+                                    //Check whether come from menu 2.
+                    if (form_menu2==true)
+                        {cout << endl << "Is this the right thing that you want to buy?"<<"\n\n\n";form_menu2=false;}
+                    else
+                        {srand(time(0)) ;cout <<endl<<"Have you change your mind?"<<endl<<"Be fast the item maybe sold out soon."<< endl<<"It is around "<< rand()%100+1<<" left!!!"<<"\n\n\n";}
+
+                    cout <<"If buy it, please press any key except \' 0 \' "<<endl<<"If want to cancel, please press \' 0 \' "<<endl << "If want to go back to the main menu, please press \' - \' and \' 1 \' "<<endl<<"and press enter.\n";
+                    cin >> no_menu;
+                    if (no_menu!=0 ||no_menu!= -1)
+                    {no_menu=4;}
+                    else if(no_menu==0){string item; double price;}
+                    }
+                }
+
+    }while (no_menu <=0 || no_menu >= 10 );
+}//while(1==1) end. The big loop that may no be escaped.
+}//main end
+
 
 
 
